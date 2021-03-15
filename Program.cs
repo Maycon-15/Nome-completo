23de233b44b8f956e8512738775a392f89f2d5bb
Maycon-15 <78987309+Maycon-15@users.noreply.github.com>
2021-03-14 22:16:58 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string sobrenome;
+            string NomeCompleto;
+            string NomeCatalogo;
+
+
+            Console.WriteLine("Digite seu primeiro nome:");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu sobrenome:");
+            sobrenome = Console.ReadLine();
+            
+            NomeCompleto = $"{nome} {sobrenome}";
+            NomeCatalogo = $"{sobrenome.ToUpper()} {nome}";
+
+            Console.WriteLine();
+            Console.WriteLine($"Nome completo: {NomeCompleto}");
+            Console.WriteLine($"Nome catálogo {NomeCatalogo}");
+
+
+        }
+    }
+}
